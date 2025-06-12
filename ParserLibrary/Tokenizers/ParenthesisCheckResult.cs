8fabb6a5b5125e1c65fa1d172f06ad9e1c4fe8c0
Pavlos Fragkiadoulakis <pavlos.fragkiadoulakis@gmail.com>
2025-06-12 16:48:24 +0300

    Add GetValidationFailures to ParenthesisCheckResult.

--- ParserLibrary/Tokenizers/ParenthesisCheckResult.cs
+++ ParserLibrary/Tokenizers/ParenthesisCheckResult.cs
@@ -1,4 +1,6 @@
-﻿namespace ParserLibrary.Tokenizers;
+﻿using FluentValidation.Results;
+
+namespace ParserLibrary.Tokenizers;
 
 public readonly struct ParenthesisCheckResult
 {
@@ -6,5 +8,27 @@ public readonly struct ParenthesisCheckResult
 
     public List<int> UnmatchedOpen { get; init; }
 
-    public bool AreMatched => UnmatchedClosed.Count == 0 && UnmatchedOpen.Count == 0;
+    public bool IsSuccess => UnmatchedClosed.Count == 0 && UnmatchedOpen.Count == 0;
+
+    public IList<ValidationFailure> GetValidationFailures()
+    {
+        //report in the order of the position of the unmatched parenthesis
+        List<(int, ValidationFailure)> failuresByPosition = [];
+
+        foreach (var unmatched in UnmatchedOpen)
+        {
+            //_logger.LogError("Unmatched open parenthesis at position {position}", unmatched);
+            //failures.Add(new ValidationFailure("Formula", $"Unmatched open parenthesis at position {unmatched}."));
+            failuresByPosition.Add((unmatched, new ValidationFailure("Formula", $"Unmatched open parenthesis at position {unmatched}.")));
+        }
+        foreach (var unmatched in UnmatchedClosed)
+        {
+            //_logger.LogError("Unmatched close parenthesis at position {position}", unmatched);
+            //failures.Add(new ValidationFailure("Formula", $"Unmatched close parenthesis at position {unmatched}."));
+            failuresByPosition.Add((unmatched, new ValidationFailure("Formula", $"Unmatched close parenthesis at position {unmatched}.")));
+        }
+        // Sort failures by position and return only failures 
+        return [.. failuresByPosition.OrderBy(e => e.Item1).Select( e => e.Item2)];
+    }
+
 } 
